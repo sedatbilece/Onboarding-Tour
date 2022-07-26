9fdb043bf9cb9de5568c03c2ef3bb5bf96196431
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-07-26 16:11:02 +0300

    .

--- src/functions/getPosition.js
+++ src/functions/getPosition.js
@@ -4,29 +4,29 @@ function getPosition(targetItem,cardwith,cardheight) {
 let list =[]
     
      //altında konumlandır !!!İYİLEŞTİRİLMELİ
-     if(targetItem[0].offsetTop+ targetItem[0].offsetHeight < cardheight/2){
+     if(targetItem.offsetTop+ targetItem.offsetHeight < cardheight/2){
 
  
-        list.push(targetItem[0].offsetLeft);
-        list.push (targetItem[0].offsetTop+ targetItem[0].offsetHeight+10);
+        list.push(targetItem.offsetLeft);
+        list.push (targetItem.offsetTop+ targetItem.offsetHeight+10);
      }
 
      else{// sağ yada sol
 
 
         // sağında konumlandır
-        if( (targetItem[0].offsetLeft+targetItem[0].offsetWidth+10+cardwith <window.innerWidth)  ){
+        if( (targetItem.offsetLeft+targetItem.offsetWidth+10+cardwith <window.innerWidth)  ){
             
-           list.push(targetItem[0].offsetLeft+targetItem[0].offsetWidth+10)
+           list.push(targetItem.offsetLeft+targetItem.offsetWidth+10)
    
-           list.push(targetItem[0].offsetTop)
+           list.push(targetItem.offsetTop)
 
        }else{//solunda konumlandır
            
    
-           list.push(targetItem[0].offsetLeft-targetItem[0].offsetWidth-10 -cardwith)
+           list.push(targetItem.offsetLeft-targetItem.offsetWidth-10 -cardwith)
    
-            list.push(targetItem[0].offsetTop)
+            list.push(targetItem.offsetTop)
        }
 
      }
